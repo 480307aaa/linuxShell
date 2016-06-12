16eecd969ea0f66a5f4ebc33a982ba9c9ab524d0
ysw <shuwei_yao@163.com>
2016-06-12 11:16:58 +0800

    shell02.sh

---
+++ shell02.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#布尔运算符
+a=10
+b=20
+
+if [ $a != $b ]
+then
+   echo "$a != $b: a is not equal to b"
+else
+   echo "$a != $b: a is equal to b"
+fi
+
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+   echo "$a -lt 100 -a $b -gt 15: returns true"
+else
+   echo "$a -lt 100 -a $b -gt 15: returns false"
+fi
+
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+   echo "$a -lt 100 -o $b -gt 100: return true"
+else
+   echo "$a -lt 100 -o $b -gt 100: return false"
+fi
+
