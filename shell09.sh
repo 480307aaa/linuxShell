7b91c5771b98d7f5846f6a46a718fb6464566a4f
ysw <shuwei_yao@163.com>
2016-07-14 19:55:28 +0800

    shell中for循环的使用

---
+++ shell09.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Shell for 循环
+#例子1：顺序输出当前列表中的数字
+for loop in 1 2 3 4 5
+do
+    echo "The value is :$loop"
+done
+#例子2：顺序输出字符串中的字符
+for str in ' This is a string'
+do
+    echo $str
+done
+#例子3：显示主目录下以.bash开头的文件
+for FILE in $HOME/.bash*
+do 
+    echo $FILE
+done
